457fa0d93c8cb4af4b367356566f37b96e1f926e
BlueBird <stm1051212@gmail.com>
2018-05-24 19:10:53 +0900

    Create issues.vue

---
+++ lnclient/src/components/tabs/transactions/issues.vue
@@ -0,0 +1,30 @@
+<template id="issuePage">
+  <v-ons-page>
+    <v-ons-toolbar>
+      <v-ons-icon icon="ion-android-arrow-back" @click="back" class="backicon" ></v-ons-icon>
+      <div class="center">Issue</div>
+    </v-ons-toolbar>
+  </v-ons-page>
+</template>
+
+<script>
+var eventBus = require('../../../eventBus.js');
+module.exports = {
+  name: 'issuePage',
+  template: '#issuePage',
+  methods : {
+    back(){
+      eventBus.$emit("back","");
+    }
+  }
+};
+</script>
+
+<style>
+.backicon{
+  position: absolute;
+  padding-left: 10px;
+  font-size: 25px;
+  padding-top: 10px;
+}
+</style>
