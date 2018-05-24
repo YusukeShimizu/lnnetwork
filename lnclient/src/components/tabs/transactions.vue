45763d2ce9327aec009e25efc0f16df1d1a1e624
BlueBird <stm1051212@gmail.com>
2018-05-24 19:08:47 +0900

    Create transactions.vue

---
+++ lnclient/src/components/tabs/transactions.vue
@@ -0,0 +1,50 @@
+<template id="transactions">
+  <v-ons-page>
+    <div v-show="transactionsShow">
+      <v-ons-list modifier="noborder">
+        <v-ons-list-header>transactions</v-ons-list-header>
+        <v-ons-list-item modifier="chevron" tappable @click = "goIssuePage">
+          issue
+        </v-ons-list-item>
+      </v-ons-list>
+    </div>
+    <div v-show="issueScreenShow">
+      <issue-page></issue-page>
+    </div>
+  </v-ons-page>
+</template>
+
+<script>
+var eventBus = require('../../eventBus.js');
+var issuePage = require('./transactions/issue.vue').default;
+
+module.exports = {
+  template : "#transactions",
+  data() {
+    return {
+      transactionsShow : true,
+      issueScreenShow : false
+    }
+  },
+  created() {
+    eventBus.$on('back',this.getback);
+  },
+  methods : {
+    goIssuePage(){
+      this.transactionsScreenShow = false;
+      this.issueScreenShow = true;
+    },
+    getback(){
+      this.settingScreenShow = true;
+      this.issueScreenShow = false;
+    }
+  },
+  components : {
+    issuePage
+  }
+};
+</script>
+
+<style>
+
+</style>
